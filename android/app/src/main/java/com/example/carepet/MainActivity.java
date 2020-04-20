88b46ffe291ff0a18626a75ee3e04f5532284d6b
mszsxq <493906387@qq.com>
2020-04-20 09:25:37 +0800

    a

---
+++ android/app/src/main/java/com/example/carepet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.carepet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
