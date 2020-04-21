d46c37d7f9367422754a2d61984b8ad5f812f27c
yanmanhuan <1807355098@qq.com>
2020-04-21 17:36:52 +0800

    k

--- android/app/src/main/java/com/example/carepet/MainActivity.java
+++ android/app/src/main/java/com/example/carepet/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.carepet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTabHost;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout Drawer;
+    private NavigationView navigationView;
+    private ImageView touxiang;
+    private RelativeLayout layout;
+    private TextView nick_phone;
+    private ImageView nick_image;
+    private TextView nick_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
         FragmentTabHost fragmentTabHost = findViewById(android.R.id.tabhost);
         fragmentTabHost.setup(this,
                 getSupportFragmentManager(),
@@ -29,8 +49,66 @@ public class MainActivity extends AppCompatActivity {
         fragmentTabHost.addTab(tabSpec3,
                 MapFragment.class,
                 null);
-
         fragmentTabHost.setCurrentTab(0);
+
+        Drawer.closeDrawer(GravityCompat.END);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                //为item设置逐个点击事件
+//               switch (menuItem.getItemId()){
+//                  case  R.id.nav_camera:
+//                      Toast.makeText(MainActivity.this,"nav_camera",Toast.LENGTH_SHORT).show();
+//                      break;
+//                }
+                /* Toast.makeText(mysellf.this,menuItem.getTitle().toString(),Toast.LENGTH_SHORT).show();*/
+                return false;
+            }
+        });
+        layout.setBackgroundColor(Color.parseColor("#B4D1E1"));
+        /*点击头像出现右侧滑抽屉*/
+        touxiang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawer.openDrawer(GravityCompat.END);
+            }
+        });
+        /*设置侧滑抽屉的header系列*/
+        View headerLayout = navigationView.inflateHeaderView(R.layout.header_layout);
+        nick_phone = (TextView)headerLayout.findViewById(R.id.nick_phone);
+        nick_name=(TextView) headerLayout.findViewById(R.id.nick_name);
+        nick_image=(ImageView)headerLayout.findViewById(R.id.nick_image);
+        nick_phone.setText("12345555");
+        nick_name.setText("可乐加冰");
+        nick_image.setImageResource(R.drawable.tx);
+        //设置图像大小时，必须先有第一句才可以进行设置
+        nick_image.setAdjustViewBounds(true);
+        nick_image.setMaxHeight(160);
+        nick_image.setMaxWidth(160);
+
+        /*点击头像跳转到上传头像界面*/
+        nick_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"跳转页面",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+    private void initView() {
+        Drawer = findViewById(R.id.da);
+        navigationView =findViewById(R.id.cebian);
+        layout=findViewById(R.id.header_layout);
+        touxiang=findViewById(R.id.touxiang);
+    }
+
+    public void onBackPressed() {
+        if (Drawer.isDrawerOpen(GravityCompat.START)) {
+            Drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
