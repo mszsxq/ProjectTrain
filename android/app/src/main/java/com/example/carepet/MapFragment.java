dc98a9a4b5574b1c5f07347db752ac881d2464dc
mszsxq <493906387@qq.com>
2020-05-30 17:12:08 +0800

    zsx

--- android/app/src/main/java/com/example/carepet/MapFragment.java
+++ android/app/src/main/java/com/example/carepet/MapFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
@@ -12,18 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.etsy.android.grid.StaggeredGridView;
-import com.example.carepet.Community.search;
-import com.example.carepet.Community.search_find;
-import com.example.carepet.adapter.FootAdapter;
 import com.example.carepet.adapter.SearchAdapter;
 import com.example.carepet.entity.FindTable;
 import com.example.carepet.entity.MapContent;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,14 +38,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 public class MapFragment extends Fragment {
     private GestureDetector gue;
     private View view;
     private  StaggeredGridView gridView;
     private List<MapContent> list = new ArrayList<>();
-    private SwipeRefreshView swipeRefreshLayout;
+    private List<MapContent> list1 = new ArrayList<>();
+    private SwipRefreshView swipeRefreshLayout;
     private ImageView mImageView;
     private TextView medittext;
+    private SearchView searchview;
+    private  String soso;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -59,8 +63,9 @@ public class MapFragment extends Fragment {
         gridView = view.findViewById(R.id.gview);
         swipeRefreshLayout=view.findViewById(R.id.swipeRefreshLayout);
         gue = new GestureDetector(getContext(), new MyGestureListener());
-        mImageView=(ImageView)view.findViewById(R.id.mimageview);
-        medittext=view.findViewById(R.id.medittext);
+//        mImageView=(ImageView)view.findViewById(R.id.mimageview);
+//        medittext=view.findViewById(R.id.medittext);
+        searchview=view.findViewById(R.id.et_ss);
     }
     public void initData(){
         MyAsnycTask myAsnycTask = new MyAsnycTask();
@@ -69,20 +74,34 @@ public class MapFragment extends Fragment {
 
     @SuppressLint("ClickableViewAccessibility")
     private void  setListner(){
-        mImageView.setOnClickListener(new View.OnClickListener() {
+//        mImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent=new Intent(getContext(), search_find.class);
+//                startActivity(intent);
+//            }
+//        });
+//        medittext.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent1=new Intent(getContext(),search_find.class);
+//                startActivity(intent1);
+//            }
+//        });
+        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getContext(), search_find.class);
-                startActivity(intent);
+            public boolean onQueryTextSubmit(String query) {
+                soso=query;
+                MyAsnycTask2 myAsnycTask = new MyAsnycTask2();
+                myAsnycTask.execute();
+                return false;
             }
-        });
-        medittext.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Intent intent1=new Intent(getContext(),search_find.class);
-                startActivity(intent1);
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
-        });
+       });
         gridView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -136,6 +155,64 @@ public class MapFragment extends Fragment {
         }
     }
 
+
+    class MyAsnycTask2 extends AsyncTask<Integer, Integer, List<FindTable>> {
+        //用来写等待时的UI（加载中转圈）
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected List<FindTable> doInBackground(Integer... integers) {
+
+//            final String ip = "http://192.168.101.16:8080/CarePet/findtable/listrecent";
+            final String ip ="http://175.24.16.26:8080/CarePet/findtable/liststrf?sousuo="+soso ;
+            URL url;
+            String json = "";
+            List<FindTable> findList = new ArrayList<>();
+            try {
+                url = new URL(ip);
+                URLConnection conn = url.openConnection();
+                InputStream in = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                json = reader.readLine();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!json.equals("")) {
+                Gson gson = new Gson();
+                findList = gson.fromJson(json, new TypeToken<List<FindTable>>() {
+                }.getType());
+            }
+            return findList;
+        }
+
+        @Override
+        protected void onPostExecute(List<FindTable> findTables) {
+            list1.clear();
+            if (findTables != null) {
+                for(FindTable f: findTables){
+                    Log.e("aaaaa", f.toString());
+                    String jason=f.getImgjson();
+                    String name =jason.split("--")[0];
+                    String touxiang =jason.split("--")[1];
+                    String bimg =jason.split("--")[2];
+                    MapContent mapContent= new MapContent();
+                    mapContent.setFindTable(f);
+                    mapContent.setName(name);
+                    mapContent.setTouxiang(touxiang);
+                    mapContent.setBimg(bimg);
+                    list1.add(mapContent);
+                }
+            }
+            //findtable 数据 不更新问题
+            SearchAdapter adapter= new SearchAdapter(getContext(), R.layout.map_search_item, list1);
+            gridView.setAdapter(adapter);
+
+        }
+    }
 
     class MyAsnycTask extends AsyncTask<Integer, Integer, List<FindTable>> {
         //用来写等待时的UI（加载中转圈）
